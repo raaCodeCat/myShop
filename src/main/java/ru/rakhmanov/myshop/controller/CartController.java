806399ab15f9e4cb1a378046e91771cec386523f
Ayrat Rakhmanov <siriustroy@yandex.ru>
2025-06-08 19:44:41 +0500

    Добавил управление количеством товара на странице корзины

--- src/main/java/ru/rakhmanov/myshop/controller/CartController.java
+++ src/main/java/ru/rakhmanov/myshop/controller/CartController.java
@@ -4,9 +4,13 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import ru.rakhmanov.myshop.dto.response.ItemDto;
 import ru.rakhmanov.myshop.service.CartService;
+import ru.rakhmanov.myshop.service.OrderItemService;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -17,6 +21,7 @@ import java.util.List;
 public class CartController {
 
     private final CartService cartService;
+    private final OrderItemService orderItemService;
 
     @GetMapping("/items")
     public String cart(Model model) {
@@ -29,6 +34,16 @@ public class CartController {
         model.addAttribute("empty", isEmpty);
 
         return "cart";
+    }
+
+    @PostMapping("/items/{id}")
+    public String editItemInCurrentOrder(
+            @PathVariable(name = "id") Long itemId,
+            @RequestParam(name = "action") String action
+    ) {
+        orderItemService.editItemInCurrentOrder(itemId, action);
+
+        return "redirect:/cart/items";
     }
 
 

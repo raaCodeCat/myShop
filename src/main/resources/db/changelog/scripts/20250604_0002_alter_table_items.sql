32aecc9cac43da46b412d14c8582790ed6612e73
Ayrat Rakhmanov <siriustroy@yandex.ru>
2025-06-08 14:49:35 +0500

    дропнул базу без данных. изменил скрипты.

--- src/main/resources/db/changelog/scripts/20250604_0002_alter_table_items.sql
+++
@@ -1,5 +0,0 @@
---liquibase formatted sql
---changeset rakhmanovaa:0002_alter_table_items stripComments:false dbms:postgresql endDelimiter:;;
---comment: 0002_alter_table_items
-
-alter table items add column image_path varchar(100);

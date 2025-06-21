a10772593b4c8b954406781a913e43aef5c8cb57
Ayrat Rakhmanov <siriustroy@yandex.ru>
2025-06-21 15:28:22 +0500

    route

--- src/main/java/ru/rakhmanov/myshop/config/WebConfig.java
+++ src/main/java/ru/rakhmanov/myshop/config/WebConfig.java
@@ -1,14 +1,19 @@
 package ru.rakhmanov.myshop.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig {
 
-    public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addRedirectViewController("/", "/main/items");
+    @Bean
+    public RouterFunction<ServerResponse> redirectRouter() {
+        return route(GET("/"),
+                req -> ServerResponse.permanentRedirect(req.uri().resolve("/main/items")).build());
     }
-
 }

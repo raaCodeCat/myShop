d1492d70b9ab4bf3d0f05a443b6fd5a2ec82d4e2
Ayrat Rakhmanov <siriustroy@yandex.ru>
2025-06-22 17:11:52 +0500

    внёс изменения в CustomExceptionHandler

--- src/main/java/ru/rakhmanov/myshop/errorhandler/CustomExceptionHandler.java
+++ src/main/java/ru/rakhmanov/myshop/errorhandler/CustomExceptionHandler.java
@@ -1,20 +1,32 @@
 package ru.rakhmanov.myshop.errorhandler;
 
-import org.springframework.ui.Model;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.reactive.result.view.Rendering;
+import reactor.core.publisher.Mono;
 import ru.rakhmanov.myshop.exeption.NotFoundException;
 
 @ControllerAdvice
 public class CustomExceptionHandler {
+
     @ExceptionHandler(NotFoundException.class)
-    public String handleNotFoundException(NotFoundException ex, Model model) {
-        model.addAttribute("message", ex.getMessage());
-        return "error404";
+    public Mono<Rendering> handleNotFoundException(NotFoundException ex) {
+        return Mono.just(
+                Rendering.view("error404")
+                        .modelAttribute("message", ex.getMessage())
+                        .status(HttpStatus.NOT_FOUND)
+                        .build()
+        );
     }
 
     @ExceptionHandler(Exception.class)
-    public String handleIternalServerError(Exception ex) {
-        return "error";
+    public Mono<Rendering> handleInternalServerError(Exception ex) {
+        return Mono.just(
+                Rendering.view("error")
+                        .modelAttribute("message", "Произошла внутренняя ошибка сервера")
+                        .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                        .build()
+        );
     }
 }
